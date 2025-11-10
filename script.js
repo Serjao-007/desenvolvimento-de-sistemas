25a0fac9b45932e69930f05598741878d4d5821c
Serjao-007 <0001134808@senaimgaluno.com.br>
2025-11-09 22:55:20 -0300

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-const link = document.querySelector('.menu a[href="#Objetivos"]');
-
-link.addEventListener('click', function (e) {
-  e.preventDefault();
-  const target = document.querySelector('#Objetivos');
-  const offset = document.querySelector('.menu').offsetHeight;
-  const pos = target.getBoundingClientRect().top + window.pageYOffset - offset;
-  window.scrollTo({ top: pos, behavior: 'smooth' });
-});
